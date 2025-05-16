9fab8ac91b85b81cfae0cdff6a5958067010bf96
Damon Delvechio <delv3chio@gmail.com>
2025-05-16 14:00:17 -0700

    Fix routing to show login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import RegistrationPage from "./pages/RegistrationPage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegistrationPage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import App from './App'; // Assuming App.jsx is your root component
-import Login from './Login';
-import Register from './pages/RegistrationPage'; // Corrected capitalization
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+createRoot(root).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/* Add other routes here */}
-          <Route path="/" element={<App />} /> {/* Assuming App is your main layout/dashboard */}
-        </Routes>
-      </BrowserRouter>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
