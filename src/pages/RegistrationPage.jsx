eeb4afddc0c6899a05bae2c5d523d5b487e839e8
Damon Delvechio <delv3chio@gmail.com>
2025-05-16 13:52:01 -0700

    Update registration file to fix export issue

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -76,4 +76,4 @@ export default function Register() {
   );
 }
 
-export default Register; // Only one default export at the end
+export default fuction RegistrationPage; // Only one default export at the end
