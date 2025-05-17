ace3f18d4239f7ca0b8045f5c092259b74a32ba2
Damon Delvechio <delv3chio@gmail.com>
2025-05-17 11:41:24 -0700

    🔧 Simplify main.jsx for render path debug

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './context/AuthProvider';
+
+console.log("✅ main.jsx starting render");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
