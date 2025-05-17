390c92c1dd4b482edc8c6ab9912ce5ae45e4d26f
Damon Delvechio <delv3chio@gmail.com>
2025-05-17 05:55:39 -0700

    🐛 Fix jwt-decode import for Vite build

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import jwtDecode from 'jwt-decode';
+import { default as jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext();
