26e04c6b216363e89dff640121703ceef8c06f2a
Damon Delvechio <delv3chio@gmail.com>
2025-05-17 06:05:21 -0700

    🛠️ Fix AuthProvider import to named export

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import AuthProvider from './context/AuthProvider';
+import { AuthProvider } from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
